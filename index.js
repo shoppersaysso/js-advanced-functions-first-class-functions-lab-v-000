73c5b2ad58c427ff2dedc6bd7cee8325511ce939
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 23:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,14 @@ const returnLastTwoDrivers = function (drivers) {
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
+const createFareMultiplier = function (multiply) {
+  return function (value) {
+    return multiply * value;
+  };
+};
+
+createFareMultiplier() — This is a higher-order function that takes in one argument, an integer,
+ and returns a function that will multiply a fare for a ride accordingly. 
+If createFareMultiplier() receives an argument of 4, it will return a function 
+that takes in a fare as an argument and quadruples the fare.
 
