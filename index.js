c5f07338ea03be247f814f369f182abc99099ca2
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 23:20:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,3 @@ const createFareMultiplier = function (multiply) {
     return multiply * value;
   };
 };
-
-createFareMultiplier() — This is a higher-order function that takes in one argument, an integer,
- and returns a function that will multiply a fare for a ride accordingly. 
-If createFareMultiplier() receives an argument of 4, it will return a function 
-that takes in a fare as an argument and quadruples the fare.
-
