c55503ef36ee3048e3ce306f5e08f44ad35f6680
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 23:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function (drivers, selection) {
-  if (selection = returnFirstTwoDrivers) {
-    returnLastTwoDrivers;
-  } else {
-    returnFirstTwoDrivers;
-  }
-}
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+  return whichDrivers(drivers);
+};
