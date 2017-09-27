ae2b368f0ebd122d64eca41ce5263c626c5b6cb7
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 23:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const returnFirstTwoDrivers = function (drivers) {
 };
 
 const returnLastTwoDrivers = function (drivers) {
-  return drivers.length - 1, drivers.length - 2;
+  return drivers.slice(-2);
 };
