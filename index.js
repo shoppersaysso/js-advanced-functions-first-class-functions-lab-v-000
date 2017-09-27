fe9c91e06bda63f1c90e117e59778b56241184ea
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 23:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
 };
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.splice(0, 2);
+};
